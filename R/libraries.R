b59e401a86bee2e0021b83338ba94cc77545959c
bmaparicio <bruno.a.aparicio@gmail.com>
2023-10-20 12:15:15 +0100

    update

---
+++ R/libraries.R
@@ -0,0 +1,22 @@
+('Loading required R packages...')
+pacman::p_load(
+  sf,
+  expp,
+  spdep,
+  rlang,
+  tidyverse
+)
+
+#
+# .onLoad = function (libname, pkgname) {
+#   datafile = sf::st_read(system.file("extdata", "stands_data.shp", package="ForSysXR"))
+#   assign('stands_data', datafile, envir = topenv())#.GlobalEnv)#envir = topenv())
+# }
+
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to ForSysXR")
+  #datafile = sf::st_read(system.file("extdata", "stands_data.shp", package="ForSysXR"))
+  #assign('stands_data', datafile, envir = topenv())#.GlobalEnv)#envir = topenv())
+}
+
