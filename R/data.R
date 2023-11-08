9b40ff22413665b6c8d689d86bea870404916055
Bruno Aparicio <bruno.a.aparicio@gmail.com>
2023-11-08 09:40:45 -0800

    new dataset with FBN

--- R/data.R
+++ R/data.R
@@ -22,3 +22,9 @@
 #' `stands_data` includes real stands from a study area with three objectives that can be optimized, an area field that can be used as constraint and one threshold. It also contains fields with the stands' availability for treatments and stands to be excluded from the analysis.
 #'
 "stands_data"
+
+#' Spatial data of stands with a fuelbreak network
+#'
+#' `stands_data_FBN` includes real stands from a study area with one objective that can be optimized, an area field that can be used as constraint and one distance to the fuelbreak network to be used as threshold. It also contains fields with the stands' availability for treatments and stands to be excluded from the analysis.
+#'
+"stands_data_FBN"
--- R/libraries.R
+++ R/libraries.R
@@ -18,6 +18,7 @@ pacman::p_load(
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Welcome to ForSysXR")
   stands_data <- sf::st_cast(stands_data,"POLYGON")
+  stands_data_FBN <- sf::st_cast(stands_data_FBN,"POLYGON")
   #datafile = sf::st_read(system.file("extdata", "stands_data.shp", package="ForSysXR"))
   #assign('stands_data', datafile, envir = topenv())#.GlobalEnv)#envir = topenv())
 }
