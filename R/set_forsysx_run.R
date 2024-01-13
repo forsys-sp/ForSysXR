cd6c322e6570917a7eecfc0aa9557c4aacda6d2c
bmaparicio <bruno.a.aparicio@gmail.com>
2024-01-12 16:56:02 -0800

    Update set_forsysx_run.R
    reports an error if any field named with more than 10 characters

--- R/set_forsysx_run.R
+++ R/set_forsysx_run.R
@@ -173,6 +173,11 @@ set_forsysx_run <- function(input_shapefile,
   }}
 
 
+  if(any(nchar(names(my_shp)))>10){
+    stop("The shapefile contains at least one field named with more than 10 characters. Please modify it manually or by using the function check_input_shapefile")
+  }
+
+
 
   input_shapefile_use <- gsub("/","\\\\\\\\",input_shapefile)
 
